9ba359c3a9286024a0e0127d3184bd66d7281fba
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-03-06 05:09:42 +0700

    Class Components React Js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const incrrect = {
+  color: "red",
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: 1,
+      num2: 1,
+      response: "",
+      incrrect: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div id="problem" style={incrrect}>
+          {this.state.num1} + {this.state.num2}
+        </div>
+        <input
+          type="text"
+          value={this.state.response}
+          onChange={this.updateResponse}
+          onKeyPress={this.inputKeyPress}
+        />
+      </div>
+    );
+  }
+
+  updateResponse = (event) => {
+    this.setState({ response: event.target.value });
+  };
+
+  inputKeyPress = (event) => {
+    if (event.key === "Enter") {
+      const answer = parseInt(event.target.value);
+      const sum = this.state.num1 + this.state.num2;
+
+      if (answer === sum) {
+        this.setState({
+          num1: Math.ceil(Math.random() * 10),
+          num2: Math.ceil(Math.random() * 10),
+          response: "",
+        });
+      } else {
+        this.setState({
+          response: "",
+          incrrect: true,
+        });
+      }
+    }
+  };
 }
 
 export default App;
